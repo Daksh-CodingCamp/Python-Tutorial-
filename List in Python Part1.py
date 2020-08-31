b3918880d81ac150b7115e39de4e866b823adf55
Daksh-CodingCamp <69832001+Daksh-CodingCamp@users.noreply.github.com>
2020-08-31 08:48:12 +0530

    Create List in Python Part1.py

---
+++ List in Python Part1.py
@@ -0,0 +1,8 @@
+list1 = ['pencil', 'pen', 'eraser', 'sharpner']
+print(type(list1))
+list2 = [45, 44, 85, 99, 100]
+list2.append("hi")
+print(list2[2:5:2])
+xyz = list2[2:5:2]
+xyz.reverse()
+print(xyz)
