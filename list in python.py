02f1dc58f3eb87011badddc641fa2b5782577f89
Daksh-CodingCamp <69832001+Daksh-CodingCamp@users.noreply.github.com>
2020-09-01 19:47:23 +0530

    Add files via upload

---
+++ list in python.py
@@ -0,0 +1,13 @@
+# list1 = ['pencil', 'pen', 'eraser', 'sharpner']
+# # print(type(list1))
+list2 = [45, 44, 85, 99, 100]
+# # list2.append("hi")
+# # print(list2[2:5:2])
+# # xyz = list2[2:5:2]
+# # xyz.reverse()
+# # print(xyz)
+# list2.sort()
+# print(list2)
+tup1 = (11, 88 , 77, 444, 79 ,778544)
+# print(type(tup1))
+print(list2)
